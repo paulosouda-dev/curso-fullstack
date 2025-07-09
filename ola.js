405e9c61f28c5071971c12a0b83d14f61c625555
macbook <macbook@MacBook-Pro---S.local>
2025-07-09 23:42:21 +0100

    Exercicio finalizado

---
+++ ola.js
@@ -0,0 +1,10 @@
+const nome = "Paulo Sousa"
+const idade = 21
+
+console.log(`Olá, ${nome}!`)
+console.log(`Tens ${idade} anos.`)
+
+const anoAtual = 2025
+const anoNascimento = anoAtual - idade
+
+console.log(`Nasceste em ${anoNascimento}.`)
