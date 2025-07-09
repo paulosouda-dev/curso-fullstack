bf68243a0c3483ce4e22f4e92aafd730b95edcee
macbook <macbook@MacBook-Pro---S.local>
2025-07-09 23:45:28 +0100

    save script cartao-visita

---
+++ cartao-visita.js
@@ -0,0 +1,9 @@
+const nome = "Paulo Sousa"
+const profissao = "Web Developer"
+const idade = 21
+
+console.log("----------------------")
+console.log(`Nome: ${nome}`)
+console.log(`Profissão: ${profissao}`)
+console.log(`Idade: ${idade}`)
+console.log("----------------------")
